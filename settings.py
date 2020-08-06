895696fefa1748b2877e0d3739ebda87f4cbbb46
lortyowusu <67658306+lortyowusu@users.noreply.github.com>
2020-08-06 22:05:03 +0000

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,19 @@
+zmq_address = '127.0.0.1:28001'
+rpc_address = '127.0.0.1:19119'
+rpc_user    = '<deleted>'
+rpc_pass    = '<deleted>'
+
+smtp_server   = "<deleted>"
+smtp_port     = 465 # For SSL
+email_pass    = "<deleted>"
+email_address = "<deleted>"
+send_email    = "<deleted>"
+
+twitter_api_key             = "<deleted>"
+twitter_api_secret_key      = "<deleted>"
+twitter_access_token        = "<deleted>"
+twitter_access_token_secret = "<deleted>"
+
+faucet_warning   = 50000.0 # Send warning tweet asking for refill
+faucet_threshold = 10000.0 # Suspend payouts
+faucet_payout    = 1000.0
